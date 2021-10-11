26da0dae87e3261cf3046cfa7e2a8fcdf525201b
jwu2018 <jyalu.wu@hotmail.com>
2021-10-11 14:25:50 -0400

    merge conflicts

--- app/src/main/java/com/example/cs4518_finalproject/LoginFragment.kt
+++ app/src/main/java/com/example/cs4518_finalproject/LoginFragment.kt
@@ -1,6 +1,7 @@
 package com.example.cs4518_finalproject
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
